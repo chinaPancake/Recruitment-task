bedee6bfbc711dea59647228b1bb8feac9685544
chinaPancake <mateusz.placek99@gmail.com>
2022-10-07 23:50:28 +0200

    new method

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
-import typer
-import requests
-from requests_html import HTMLSession
-import lxml
-import json
 import httplib2
-from bs4 import BeautifulSoup, SoupStrainer
+import typer
 import csv
+from bs4 import BeautifulSoup, SoupStrainer
+
 
 # we have to crawl: link, title, number of internal links, number of times url was referenced by other pages
 # crawl --page <URL> --format <csv/json> --output <path_to_file>
 
 # done link
 # done title
+# counting internal and external links is done
 
 class ToCrawl:
     def __init__(self, url: str):
         # input url
+        self.external = None
         self.url = url
         self.http = httplib2.Http()
+
+        # get request form URL
         self.status, self.response = self.http.request(self.url)
         self.count_external = 0
+
+        # all links array
         self.all_links = []
+        self.all_titles = []
+
+        # get title from URL
+        for link in BeautifulSoup(self.response, 'html.parser', parseOnlyThese=SoupStrainer('title')):
+            self.all_titles.append(link.text)
+            print(self.all_titles)
 
 
         for link in BeautifulSoup(self.response, 'html.parser', parseOnlyThese=SoupStrainer('a')):
             if link.has_attr('href'):
                 self.all_links.append(link['href'])
 
-        for self.external in self.all_links:
-            if self.external.startswith('https') or self.external.startswith('http'):
-                self.count_external += 1
+        for link in self.all_links:
+            if self.is_external_link(link=link):
+                self.count_external +=1
 
         print(f'There is: {len(self.all_links)}, links')
         print(f'There is: {self.count_external} external links')
         print(f'There is: {len(self.all_links) - self.count_external} linternal links')
+
+    def is_external_link(self, link: str) -> bool:
+        return link.startswith('https') or link.startswith('http')
+
+    def save_to_file(self):
+        # save to CSV/JSON file with link, title, len(self.all_link), self.count_external, len(self.all_links) - self.count_external
+
+        # im doing resaerch on github and stackoverflow right now,
+
+        with open('csv_file.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(self.all_links)
+
+        pass
 
 if __name__ == "__main__":
     typer.run(ToCrawl)
