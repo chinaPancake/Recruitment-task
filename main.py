a28cc4741a12775962c0bfbadf7f7f7333398e4b
chinaPancake <mateusz.placek99@gmail.com>
2022-10-09 14:45:44 +0200

    getting all interal url is working, fixing colony title

--- main.py
+++ main.py
@@ -22,46 +22,60 @@ class ToCrawl:
 
         # get request form URL
         self.status, self.response = self.http.request(self.url)
-        self.count_external = 0
 
         # all links array
         self.all_links = []
         self.all_titles = []
         self.internal_links = []
+        self.external_links = []
 
         # get title from URL
         for link in BeautifulSoup(self.response, 'html.parser', parseOnlyThese=SoupStrainer('title')):
             self.all_titles.append(link.text)
             print(self.all_titles)
 
-
         for link in BeautifulSoup(self.response, 'html.parser', parseOnlyThese=SoupStrainer('a')):
             if link.has_attr('href'):
                 self.all_links.append(link['href'])
 
-        self.counter = 0
+
         for link in self.all_links:
             if self.is_external_link(link=link):
-                self.count_external +=1
+                self.external_links.append(link)
             else:
-                if link.startswith('/'):
-                    self.internal_links.append(self.url+link)
+                self.internal_links.append(self.url + link)
 
-        try:
-            for link in self.internal_links:
+        for link in self.internal_links:
+            try:
                 self.go_to = httplib2.Http()
                 self.go_to_status, self.go_to_response = self.go_to.request(link)
                 for title in BeautifulSoup(self.go_to_response, 'html.parser', parseOnlyThese=SoupStrainer('title')):
                     self.all_titles.append(title.text)
-        except (RedirectLimit, httplib2.ServerNotFoundError, UnicodeError, httplib2.RelativeURIError):
-            print('redirection limit on', link)
+                    print(self.all_titles)
+            except (RedirectLimit, httplib2.ServerNotFoundError, UnicodeError, httplib2.RelativeURIError):
+                print('redirection limit on', link)
+
+
+        self.save_to_file()
+
+            # for link in self.internal_links:
+            #     self.go_to = httplib2.Http()
+            #     self.go_to_status, self.go_to_response = self.go_to.request(link)
+            #     for title in BeautifulSoup(self.go_to_response, 'html.parser', parseOnlyThese=SoupStrainer('title')):
+            #         try:
+            #             self.all_titles.add(title.text)
+            #             print(self.all_titles)
+            #         except (RedirectLimit, httplib2.ServerNotFoundError, UnicodeError, httplib2.RelativeURIError):
+            #             print('redirection limit on', link)
+
+
 
 
         print(f'There is: {len(self.all_links)}, links')
-        print(f'There is: {self.count_external} external links')
-        print(f'There is: {len(self.all_links) - self.count_external} linternal links')
+        print(f'There is: {len(self.external_links)} external links')
+        print(f'There is: {len(self.internal_links)} internal links')
+        print(f'There is: {len(self.all_titles)} titles')
         print(self.internal_links)
-        self.save_to_file()
 
     def is_external_link(self, link: str) -> bool:
         return link.startswith('https') or link.startswith('http')
@@ -69,8 +83,9 @@ class ToCrawl:
     def save_to_file(self):
         # save to CSV/JSON file with link, title, len(self.all_link), self.count_external, len(self.all_links) - self.count_external
         self.file = open('datas.json', 'w')
-        json.dump(self.all_titles, self.file)
+        json.dump(self.internal_links, self.file)
         self.file.close()
+
 
 if __name__ == "__main__":
     typer.run(ToCrawl)
